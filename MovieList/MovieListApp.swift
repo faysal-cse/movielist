3c2d59718c3f940c91483cc44b4546dcd28ed554
Faysal <shikder.ahmed@ssd-tech.io>
2022-10-10 22:31:22 +0600

    Initial Commit

---
+++ MovieList/MovieListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieListApp.swift
+//  MovieList
+//
+//  Created by Faysal on 10/10/22.
+//
+
+import SwiftUI
+
+@main
+struct MovieListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
